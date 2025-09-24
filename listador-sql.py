ccfadfb1ea173c524cf52d62a7db8aaf453c8a65
Agustin Bustos Piasentini (Nitro) <63634574+abustosp@users.noreply.github.com>
2025-09-24 17:40:50 -0300

    fix: Rename SQL files to replace spaces with underscores and update file list

--- listador-sql.py
+++ listador-sql.py
@@ -12,6 +12,14 @@ sqlpassword = os.getenv("MYSQL_ROOT_PASSWORD")
 directorio = "master"
 archivos = os.listdir(directorio)
 archivos = [archivo for archivo in archivos if archivo.endswith('.sql')]
+# Reemplazar espacios por _ en los nombres de archivos originales y renombrarlos en el sistema de archivos
+for archivo in archivos:
+    nuevo_nombre = archivo.replace(' ', '_')
+    if archivo != nuevo_nombre:
+        os.rename(os.path.join(directorio, archivo), os.path.join(directorio, nuevo_nombre))
+
+# Actualizar la lista de archivos con los nuevos nombres
+archivos = [archivo.replace(' ', '_') for archivo in archivos]
 # Eliminar el archivo creardatabases.sql si existe
 if 'creardatabases.sql' in archivos:
     archivos.remove('creardatabases.sql')
@@ -21,6 +29,7 @@ importar = []
 for i in archivos:
     # imprimir la database (es lo que precede a "Database: ") (está dentro de las primeras 5 línes)
     i = i.replace('\\', '/')
+    print(f'Procesando archivo: {i}')
     with open(i, 'r') as f:
         for j in range(5):
             linea = f.readline()
