664ef27ac764a485f8aeca4aaa99dabdac116bd4
Agustin Bustos Piasentini (Nitro) <63634574+abustosp@users.noreply.github.com>
2025-09-24 17:56:47 -0300

    feat: Add backup functionality to sql-backupper.py and update .dockerignore and .gitignore to include backups

--- sql-backupper.py
+++ sql-backupper.py
@@ -2,6 +2,7 @@ import os
 import re
 from dotenv import load_dotenv
 from datetime import datetime
+import mysql.connector
 
 
 # leer variables de entorno
@@ -21,19 +22,34 @@ hoy_path = os.path.join(directorio, hoy_carpeta)
 os.makedirs(hoy_path, exist_ok=True)
 
 # obtener la lista de bases de datos
-comando_listar_bases = f'docker exec mysql bash -c "mariadb -uroot -p{sqlpassword} -e SHOW DATABASES;"'
-bases_de_datos = os.popen(comando_listar_bases).read().strip().split("\n")[1:]
-bases_de_datos = [db for db in bases_de_datos if db not in EXCLUDE_DBS]
+
+# Conectar a la base de datos MySQL usando mysql-connector-python
+conn = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password=sqlpassword
+)
+cursor = conn.cursor()
+cursor.execute("SHOW DATABASES;")
+bases_de_datos = [db[0] for db in cursor.fetchall() if db[0] not in EXCLUDE_DBS and db[0] != "DATABASES"]
+cursor.close()
+conn.close()
+bases_de_datos = [db for db in bases_de_datos if db not in EXCLUDE_DBS and db != "DATABASES"]
+
+print("---------------------------------------")
 print("Bases de datos a respaldar:", bases_de_datos)
+print("---------------------------------------")
+
 # hacer el respaldo de cada base de datos
 for db in bases_de_datos:
-    nombre_archivo = f"{db}.sql"
+    nombre_archivo = f"{db}_{hoy_carpeta}.sql"
     ruta_completa = os.path.join(hoy_path, nombre_archivo)
     comando_crear_carpeta = f'docker exec mysql bash -c "mkdir -p /backups/{hoy_carpeta}"'
     comando_backup = f'docker exec mysql bash -c "mariadb-dump -uroot -p{sqlpassword} {db}" > "{ruta_completa}"'
     os.system(comando_crear_carpeta)
     os.system(comando_backup)
     print(f"Respaldo de {db} completado y guardado en {ruta_completa}")
+print("---------------------------------------")
 print("Respaldo de todas las bases de datos completado.")
 
 
