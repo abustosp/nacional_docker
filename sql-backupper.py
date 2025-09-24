864210b854b3cc7219b265a64949e5cc593bd885
abp-server-nacional <abp-server-nacional>
2025-09-24 22:13:42 +0200

    fix: Correct Docker command syntax for database listing and backup

--- sql-backupper.py
+++ sql-backupper.py
@@ -21,7 +21,7 @@ hoy_path = os.path.join(directorio, hoy_carpeta)
 os.makedirs(hoy_path, exist_ok=True)
 
 # obtener la lista de bases de datos
-comando_listar_bases = f'docker exec mysql bash -c "mariadb -u root -p{sqlpassword} -e SHOW DATABASES;"'
+comando_listar_bases = f'docker exec mysql bash -c "mariadb -uroot -p{sqlpassword} -e SHOW DATABASES;"'
 bases_de_datos = os.popen(comando_listar_bases).read().strip().split("\n")[1:]
 bases_de_datos = [db for db in bases_de_datos if db not in EXCLUDE_DBS]
 print("Bases de datos a respaldar:", bases_de_datos)
@@ -29,7 +29,9 @@ print("Bases de datos a respaldar:", bases_de_datos)
 for db in bases_de_datos:
     nombre_archivo = f"{db}.sql"
     ruta_completa = os.path.join(hoy_path, nombre_archivo)
-    comando_backup = f'docker exec mysql bash -c "mariadb-dump -u root -p{sqlpassword} {db}" > "{ruta_completa}"'
+    comando_crear_carpeta = f'docker exec mysql bash -c "mkdir -p /backups/{hoy_carpeta}"'
+    comando_backup = f'docker exec mysql bash -c "mariadb-dump -uroot -p{sqlpassword} {db}" > "{ruta_completa}"'
+    os.system(comando_crear_carpeta)
     os.system(comando_backup)
     print(f"Respaldo de {db} completado y guardado en {ruta_completa}")
 print("Respaldo de todas las bases de datos completado.")
